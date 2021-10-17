4407ab1f1279011437184f97103473d9e4de16a2
Justice Ohene Amofa <70078600+Joamofa1@users.noreply.github.com>
2021-10-17 18:59:13 +0000

    starter pack

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        For a = 0 To 100
+            If a Mod 2 <> 0 Then
+                ListBox1.Items.Add(a)
+            End If
+        Next
+    End Sub
+End Class
